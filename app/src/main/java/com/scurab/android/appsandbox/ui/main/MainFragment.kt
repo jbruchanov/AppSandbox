0204f36495d82b66ac51464f5b316c66f38a195c
Jiri Bruchanov <jbruchanov@gmail.com>
2020-02-16 21:19:45 +0000

    Add navigation framework

--- app/src/main/java/com/scurab/android/appsandbox/ui/main/MainFragment.kt
+++ app/src/main/java/com/scurab/android/appsandbox/ui/main/MainFragment.kt
@@ -1,23 +1,41 @@
 package com.scurab.android.appsandbox.ui.main
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.scurab.android.appsandbox.R
+import kotlinx.android.parcel.Parcelize
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
+    private lateinit var viewModel: MainViewModel
+    private val sampleArgs: SampleArgs? by lazy {
+        arguments?.let {
+            MainFragmentArgs.fromBundle(it).sampleArgs
+        }
     }
 
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sampleArgs?.let { args ->
+            view.findViewById<TextView>(R.id.message).apply {
+                append("\n${args.name}")
+            }
+        }
+
+        view.findViewById<View>(R.id.button).setOnClickListener { testNavigation() }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -26,4 +44,15 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    private fun testNavigation() {
+        val actionMainFragmentToMainFragment2 =
+            MainFragmentDirections.actionMainFragmentToMainFragment2(SampleArgs("Arguments passed", 1))
+        findNavController().navigate(actionMainFragmentToMainFragment2)
+    }
 }
+
+@Parcelize
+class SampleArgs(
+    val name: String,
+    val value: Int
+) : Parcelable
